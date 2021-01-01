d5c06520f31e0ff7f5627f475089a3e95b94eb9f
toru-Major <tohru7718@gmail.com>
2021-01-01 15:36:51 +0900

    Planモデルを再作成
    Issue4の解決に取り組む中で、Planモデルを削除してしまったため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_072724) do
+ActiveRecord::Schema.define(version: 2021_01_01_062612) do
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "plan"
