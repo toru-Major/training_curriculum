672444912e6e288366b839db6b36b4620892edfb
toru-Major <tohru7718@gmail.com>
2021-01-04 15:31:18 +0900

    Issue4　再修正
    Issue4の二回目のcommitになります

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,6 +3,7 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
+    @plan = Plan.new
   end
 
   # 予定の保存
@@ -14,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
