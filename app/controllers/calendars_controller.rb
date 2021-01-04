882e2d2a04c22d8b72ea94c2a9789c35f489ad6e
toru-Major <tohru7718@gmail.com>
2021-01-04 12:23:23 +0900

    Revert "Issue4　二回目"
    This reverts commit 2790d56a4cc028567eddb67eb3fe3b5551eabe33.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,18 +3,19 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
+    @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    @plan = Plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
